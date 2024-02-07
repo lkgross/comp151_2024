ab855e025de49afde66ba7514ed756e11c523b3d
lkgross <profgross@gmail.com>
2024-02-07 12:20:17 -0500

    Includes more work on lists

--- comp151_2024_01_31_lists.py
+++ comp151_2024_01_31_lists.py
@@ -19,3 +19,54 @@ print(f"The element at the end of roster is {roster[-1]}.")
 # We can find the length of a list.
 print(len(roster))
 print(f"The length of list1 is {len(list1)}.")
+print(type(list1))
+print(type((list1[0])))
+
+# Call the sorted function to return the (temporarily) sorted list.
+print(sorted(friends))
+# The original list is unchanged.
+print(friends)
+
+print(sorted(friends, reverse=True))
+print(roster)
+# We index into the list with square brackets [] to access
+# an element in the list.
+print(f"The element at index 4 in roster is {roster[4]}.")
+# We can index into a string to access a character in the
+# string.
+print('andrew'[4])
+# Use indexing to modify the elements in the list.
+roster[3] = 'john'
+# Lists are mutable: They can be changed with indexing.
+print(roster)
+
+print()
+
+student_name = 'andrew'
+# We can't use indexing to modify the elements of a string.
+# student_name[4] = 'y'
+# Strings are immutablel
+print(student_name)
+
+print()
+
+# Overwrite the element second from the end with 'ronald'.
+print(roster[-2])
+roster[-2] = 'ronald'
+print(roster)
+
+print()
+
+# Add or append 'alex' onto the end of the list.
+roster.append('alex')
+# The original list is changed.
+print(roster)
+
+print()
+
+# We can insert an element at a certain index.
+roster.insert(1, 'luis')
+# The original list is changed.
+print(roster)
+
+print(sorted([3, 17, -1, 0, 5, 12, -6]))
