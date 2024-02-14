c16b5959337a104df31393397220bb7e4caca7f8
lkgross <profgross@gmail.com>
2024-02-14 15:12:25 -0500

    Add some more concepts

--- comp151_2024_02_07.py
+++ comp151_2024_02_07.py
@@ -1,3 +1,6 @@
+# Discussion about returned values
+# Exploration of lists
+
 # The function type returns a value.
 type("Hello")
 # To see the value, wrap a print around the call to type.
@@ -56,7 +59,7 @@ print(f"The sorted version was stored in countries_sorted:"
 print()
 
 # The append method adds an element to the end of a list.
-# It does return a value.
+# It does not return a value.
 countries.append('Cabo Verde')
 print(f"After calling the append method on countries, "
       f"the list is changed: {countries}")
@@ -79,4 +82,87 @@ max(num_list)
 # To see the returned value, wrap a print around the call to max.
 print(max(num_list))
 
+print()
+
+print(f"The sum of the elements is {sum(num_list)}.")
+
+print()
+
+student = "YI yi SUN"
+# The title method returns a value.
+print("The returned value can be printed or otherwise used:")
+print(student.title())
+
+print(f"After calling the title method on student, the value of student remains unchanged: {student}.")
+print("Unlike lists, strings are not mutable.")
+
+print()
+
+print("Delete an element by index using the del keyword.")
+print(countries_sorted)
+del countries_sorted[-1]
+print(countries_sorted)
+
+print()
+
+print(countries)
+# The remove method does not return a value.
+print(countries.remove("US"))
+# You can see that no value was returned (except for None).
+print()
+print(countries)
+
+# None is its own data type.
+print(type(None))
+
+countries.append("Ukraine")
+countries.append("Brazil")
+
+print()
+print(countries)
+
+print()
+
+countries.remove("Brazil")
+print(countries)
+
+print()
+countries.insert(1, "US")
+print(countries)
+
+del countries[1]
+print()
+print(countries)
+
+print()
+countries.insert(1, "US")
+print(countries)
+# The pop method called with no parameters RETURNS the last element (pops it from the end of the list.
+print(countries.pop())
+print(countries)
+print("Pop from index 1:")
+print(f"The returned value is {countries.pop(1)}.")
+print(countries)
+print("Pop removes an element. It modifies the list.")
+
+print()
+
+print("Slicing is for working with a subset of a list.")
+print(countries_sorted)
+countries_sorted.append("US")
+countries_sorted.insert(0, "US")
+print(countries_sorted)
+# Slice from index 1 up to and not including 6:
+countries_sorted = countries_sorted[1:6]
+print(countries_sorted[1:6])
+
+print()
+day = ["train", "meeting", "comp151", "comp150", "drop-in hours", "train"]
+print(day[2:4])
+
+print()
+# Make a copy of the list in memory with this syntax.
+print(countries_sorted)
+copy_of_countries_sorted = countries_sorted[:]
+# Don't omit the [:]. Don't do a reference copy!
 
---
+++ comp151_2024_02_14_lists_and_loops.py
@@ -0,0 +1,52 @@
+family = ["Donald", "Mercedes", "Chandra", "Suzanne"]
+print("The family members are:")
+for member in family:
+    print(member)
+print("There are four members.")  # The for loop is no longer being used.
+
+print()
+
+print("The family members are:")
+for member in family:
+    print(member)
+    print("is one member.")  # The for loop is still being used.
+print("There are four members.")  # The for loop is still being used.
+
+print()
+for number in range(11):
+    print(number)
+
+print()
+
+for i in range(11):
+    print(i)
+
+print()
+
+# Print "Hello!" eleven times
+for number in range(11):
+    print("Hello!")
+
+# The function range starts a sequence from 0 by default.
+# We saw range(11) is a sequence of 11 numbers that starts from 0
+# and goes up to and including 10.
+# The function range can also take another starting value other than 0.
+for number in range(1, 11):
+    print(number)
+
+print()
+
+for number in range(1, 11, 2):
+    print(number)
+
+print()
+
+# Another to print 1 to 10 inclusive.
+for i in range(10):
+    print(i + 1)
+
+print()
+
+# Another to print 1, 3, 5, 7, 9.
+for i in range(5):
+    print(1 + 2 * i)
