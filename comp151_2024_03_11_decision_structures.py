fed08bb175b1ec89c6f97526a2ca8550e1632120
lkgross <profgross@gmail.com>
2024-03-18 15:12:02 -0400

    More on decisions

--- comp151_2024_03_11_decision_structures.py
+++ comp151_2024_03_11_decision_structures.py
@@ -12,4 +12,36 @@ print(3 > 4)
 classes = ['COMP151', 'COMP152', 'COMP250']
 print('COMP152' in classes)
 print('MATH095' in classes)
-print('a' in 'Laura')
+print('au' in 'Laura')
+
+print()
+
+# Explore if statements
+print("Exploration of if")
+age = 55
+if age > 54:
+    print("You're older than your professor.")
+if age < 54:
+    print("You're younger than your professor.")
+if age == 54:
+    print("You're the same age as your professor.")
+
+print()
+
+print("Here is an if-else")
+if age < 54:
+    print("You are younger than your professor.")
+else:
+    print("You are not younger than your professor.")
+
+print()
+print("Here is an if-elif-else chain")
+if age > 54:
+    print("You are older than your professor.")
+elif age < 54:
+    print("You are younger than your professor.")
+else:
+    print("You are the same age as your professor.")
+
+
+
