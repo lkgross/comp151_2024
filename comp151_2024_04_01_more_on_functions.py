5696b1d151f1d65cd9f4ced0dcc10d7a8aa6be8b
lkgross <profgross@gmail.com>
2024-04-03 13:39:39 -0400

    Use more than one parameter; return a value

--- comp151_2024_04_01_more_on_functions.py
+++ comp151_2024_04_01_more_on_functions.py
@@ -1,3 +1,6 @@
+import random
+
+
 def more_points():
     """Print whether the player has more points than the dealer or not."""
     player_points = 17
@@ -10,24 +13,58 @@ def more_points():
 
 # The function is defined with a parameter NAME player_points.
 def more_points_than_dealer(player_points):
-    """Print whether the player has more than the dealer's 19 or not."""
+    """Print whether the player has more points than the dealer's 19 or not."""
     if player_points > 19:
         print("The player has more points than the dealer.")
     else:
         print("The player does not have more points than the dealer.")
 
 
-def yell(mascot):
-    for i in range(3):
+def yell(mascot, repetitions):
+    """Print a cheer for mascot."""
+    for i in range(repetitions):
         print(f"Go!\nGo, {mascot}!")
 
 
 def cheers():
-    yell("Yankees!")
+    """Call yell several times."""
+    yell("Yankees!", 1)
     print()
-    yell("Bears!")
+    yell("Bears!", 3)
+    print()
+    yell("Jumbos!", 3)
 
 
+def more_points_than_dealer2(player_points, dealer_points):
+    """Print whether the player has more points than the dealer."""
+    if player_points > dealer_points:
+        print("The player has more points than the dealer.")
+    else:
+        print("The player does not have more points than the dealer.")
+
+
+def player_has_more(player_points, dealer_points):
+    return player_points > dealer_points
+
+
+def pick_a_name(roster):
+    # Variables are local to the function in which they are defined.
+    index = random.randrange(len(roster))
+    print(f"I pick {roster[index]}!")
+
+
+def gpa(As, Bs, Cs, Ds, Fs):
+    """Return the GPA for a person who earns As A grades, Bs B grades, Cs C grades
+    Ds D grades and Fs F grades in classes that are all worth the same number of credits."""
+    return (4.0 * As + 3.0 * Bs + 2.0 * Cs + 1.0 * Ds) / (
+                As + Bs + Cs + Ds + Fs)
+
+
+print(gpa(2, 2, 1, 0, 0))
+print(player_has_more(17, 19))
+
+# pick_a_name(["Nathan", "Jared", "Jack", "Luiz", "Alex"])
+# print(index)
 # The function is not actually saying anything yet.
 # more_points()
 # The function is called or invoked with the parameter VALUE 20.
@@ -38,4 +75,7 @@ def cheers():
 # more_points_than_dealer(22)
 # more_points_than_dealer(17)
 # yell("Pandas")
-cheers()
+# cheers()
+
+# more_points_than_dealer2(17, 19)
+# cheers()
