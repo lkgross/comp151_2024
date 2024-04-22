b1230497e1ae9daaea06588e19e7c636cf45f2a7
lkgross <profgross@gmail.com>
2024-04-22 13:17:52 -0400

    Work more with data files

--- comp151_2024_04_17_files.py
+++ comp151_2024_04_17_files.py
@@ -1,16 +1,17 @@
-filename = 'siddhartha'
+def read_file():
+    filename = 'siddhartha'
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+    print(contents)
 
-with open(filename) as f_obj:
-    contents = f_obj.read()
 
-print(contents)
+def read_lines():
+    filename2 = 'student_names'
+    with open(filename2) as f_obj:
+        # contents = f_obj.read()
+        for line in f_obj:
+            print(line.rstrip())
 
-filename2 = 'student_names'
 
-with open(filename2) as f_obj:
-    # contents = f_obj.read()
-    for line in f_obj:
-        print(line.rstrip())
-
-# print(contents)
-
+# read_file()
+# read_lines()
